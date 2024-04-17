ea1b113cdc010317b3689528cf3b561fc48aa59e
João Victor <joaovictorsantos10@hotmail.com.br>
2024-04-17 17:29:06 -0300

    questao 03 resolvida

--- src/Questao03.java
+++ src/Questao03.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Questao03 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        int[] numeros = new int[8];
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Digite um número: ");
+            numeros[i] = in.nextInt();
+        }
+
+        for (int n : numeros) {
+            if (n > 0)
+                pos.add(n);
+            else
+                neg.add(n);
+        }
+
+        int[] positivos = new int[pos.size()];
+        int[] negativos = new int[neg.size()];
+
+        for (int i = 0; i < pos.size(); i++) {
+            positivos[i] = pos.get(i);
+        }
+        for (int i = 0; i < neg.size(); i++) {
+            negativos[i] = neg.get(i);
+        }
+
+        System.out.println("\nNúmeros positivos:");
+        for (int n : positivos) {
+            System.out.print(n + " | ");
+        }
+        System.out.println("\nNúmeros negativos:");
+        for (int n : negativos) {
+            System.out.print(n + " | ");
+        }
+
+
+
+        in.close();
     }
 }
