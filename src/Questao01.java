f0e92aaac0a5272efccc6ea4c33dac2ab092b672
João Victor <joaovictorsantos10@hotmail.com.br>
2024-04-17 17:13:05 -0300

    add questao 03

--- src/Questao01.java
+++ src/Questao01.java
@@ -4,7 +4,21 @@ public class Questao01 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        int[] numeros = new int[9];
 
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Informe o número " + (i + 1) + ":");
+            numeros[i] = in.nextInt();
+        }
+
+        int primo = 0;
+        for (int n : numeros) {
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    primo++;
+                }
+            }
+        }
 
 
 
---
+++ src/Questao03.java
@@ -0,0 +1,5 @@
+public class Questao03 {
+    public static void main(String[] args) {
+
+    }
+}
