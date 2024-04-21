d9ce57c94566b5f55bd2b51c16fae1c7c5109ecd
João Victor <joaovictorsantos10@hotmail.com.br>
2024-04-21 19:47:47 -0300

    questao 01 resolvida

--- src/Questao01.java
+++ src/Questao01.java
@@ -1,11 +1,29 @@
+//Faça um programa que preencha um vetor com nove números inteiros, calcule e mostre os números primos e suas respectivas posições.
+
 import java.util.Scanner;
 
 public class Questao01 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.println("Informe nove números:");
+        int[] numeros = new int[9];
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = in.nextInt();
+        }
 
+        int primo = 0;
 
+        System.out.println("Os números primos e suas respectivas posições são:");
+        for (int i = 0; i < numeros.length; i++) {
+            primo = 0;
+            for (int j = 1; j <= numeros[i]; j++) {
+                if (numeros[i] % j == 0)
+                    primo++;
+            }
+            if (primo == 2)
+                System.out.println(numeros[i] + " posição " + (i + 1));
+        }
 
 
         in.close();
--- src/Questao03.java
+++ src/Questao03.java
@@ -1,3 +1,8 @@
+/* Faça um programa que preencha um vetor com oito números inteiros, calcule e mostre dois vetores resultantes. O primeiro vetor
+ * resultante deve conter os números positivos e o segundo, os números negativos. Cada vetor resultante vai ter, no máximo, oito posições,
+ * que não poderão ser completamente utilizadas.
+ */
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
